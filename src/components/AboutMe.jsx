46025be2e9a942c52a610c0b5f6f535d79483143
edscodes <edscodes@gmail.com>
2024-11-27 22:29:42 -0500

    ADD, animated tittle to about me section, pending fix some issues

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,16 +1,26 @@
-import logoRecortadoEdsCodes from '../assets/logoRecortadoEdsCodes.jpg'
+import logoRecortadoEdsCodes from '../assets/logoRecortadoEdsCodes.jpg';
 
 function AboutMe() {
     return (
         <div className="container-fluid">
             <div className="row aboutMeContainer align-items-center">
-                <div className="aboutMe col-12 col-md-6 d-flex justify-content-start mt-3 mt-md-0">
-                    <div className="d-flex flex-column align-items-center text-center">                    
-                        <h3 className=''>I'm John Edward Gonzalez</h3>
+                <div className="aboutMe col-12 col-md-6 d-flex justify-content-start mt-3 mt-md-5">
+                    <div className="d-flex flex-column align-items-center text-center">
                         <div className="aboutMeParagraph text-start fs-5 lh-lg">
+                            <h2 className="">
+                                <div className="content">
+                                    <div className="visible d-flex flex-row">
+                                        <p className=''>About</p>
+                                        <ul>
+                                            <li>Me</li>
+                                            <li>John</li>
+                                            <li>edsCodes</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </h2>
                             <p>  
                                 Frontend developer with experience in the banking sector, specialized in React and Angular. I have strong skills in HTML, CSS, and JavaScript, along with fluent English for collaborating on international projects. Passionate about creating efficient and intuitive web solutions.
-                                {/* Desarrollador frontend con experiencia en el sector bancario, especializado en React y Angular. Tengo habilidades sólidas en HTML, CSS, JavaScript, y manejo fluido de inglés para colaborar en proyectos internacionales. Apasionado por crear soluciones web eficientes e intuitivas. */}
                             </p>
                         </div>
                     </div>
@@ -27,7 +37,11 @@ function AboutMe() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default AboutMe
+export default AboutMe;
+
+
+
+{/* Desarrollador frontend con experiencia en el sector bancario, especializado en React y Angular. Tengo habilidades sólidas en HTML, CSS, JavaScript, y manejo fluido de inglés para colaborar en proyectos internacionales. Apasionado por crear soluciones web eficientes e intuitivas. */}
