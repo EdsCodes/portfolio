a3df32b9d75d603410affcf44bedf4ddcfec464e
edscodes <edscodes@gmail.com>
2024-11-28 22:24:47 -0500

    ADD, .interactiveAboutMe inside the remarked card of the aboutMe

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,10 +7,10 @@ function AboutMe() {
                 <div className="aboutMe col-12 col-md-6 d-flex justify-content-start mt-3 mt-md-5">
                     <div className="d-flex flex-column align-items-center text-center">
                         <div className="aboutMeParagraph text-start fs-5 lh-lg">
-                            <h2 className="">
+                            <h2 className="interactiveAboutMe position-relative">
                                 <div className="content">
                                     <div className="visible d-flex flex-row">
-                                        <p className=''>About</p>
+                                        <p className="about">About</p>
                                         <ul>
                                             <li>Me</li>
                                             <li>John</li>
@@ -19,18 +19,18 @@ function AboutMe() {
                                     </div>
                                 </div>
                             </h2>
-                            <p>  
+                            <p>
                                 Frontend developer with experience in the banking sector, specialized in React and Angular. I have strong skills in HTML, CSS, and JavaScript, along with fluent English for collaborating on international projects. Passionate about creating efficient and intuitive web solutions.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="edscodesLogo col-12 col-md-6 d-flex justify-content-end">
-                    <div className="d-flex flex-column align-items-center"> 
-                        <img 
-                            src={logoRecortadoEdsCodes} 
-                            className="photo img-fluid" 
-                            style={{ maxHeight: "18.4em", width: "auto" }} 
+                    <div className="d-flex flex-column align-items-center">
+                        <img
+                            src={logoRecortadoEdsCodes}
+                            className="photo img-fluid"
+                            style={{ maxHeight: "18.4em", width: "auto" }}
                             alt="edsCodesLogo"
                         />
                     </div>
@@ -41,7 +41,5 @@ function AboutMe() {
 }
 
 export default AboutMe;
-
-
 
 {/* Desarrollador frontend con experiencia en el sector bancario, especializado en React y Angular. Tengo habilidades sólidas en HTML, CSS, JavaScript, y manejo fluido de inglés para colaborar en proyectos internacionales. Apasionado por crear soluciones web eficientes e intuitivas. */}
