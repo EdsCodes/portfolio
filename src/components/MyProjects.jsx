1781c0be3092258e6a568e42ce01ab992ac69b3c
edscodes <edscodes@gmail.com>
2024-12-08 22:46:01 -0500

    FIX, myProjects component. ADD, some links and images

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,4 +1,5 @@
-
+import imgEcommerceHtmlCssJs from '../assets/imgs/Ecommerce HTML CSS & Js.png'
+import imgEcommerceReact from '../assets/imgs/Ecommerce React.png'
 
 function MyProjects () {
     return (
@@ -7,60 +8,39 @@ function MyProjects () {
                 <h2 className="projectsTittle mb-5">My projects</h2>
             </div>
             <div className="row">
-                <div className="skill col-2">
+                <div className="skill col-4">
                     <div className="project">
                         <div className="projectImg">
                             <a href="https://edscodes.github.io/E-Commerce-De-Ropa/index.html">
-                                <iframe src="https://edscodes.github.io/E-Commerce-De-Ropa/index.html" width="200" height="auto"></iframe>
+                                <img src={imgEcommerceHtmlCssJs} alt="Vista previa eccomerce con HTML+CSS+JS" target="_blank" rel="noopener noreferrer"></img>
                             </a>
                         </div>
                         <div className="projectName">
-                            <h3>E-commerce HTML+CSS+Js</h3>
+                            <h3 className='mt-5 text-center'>E-commerce HTML+CSS+JS</h3>
                         </div>
                     </div>
                 </div>
-                <div className="skill col-2">
+                <div className="skill col-4">
                     <div className="project">
                         <div className="projectImg">
+                            <a href="https://proyecto-final-gonzalez-coder.netlify.app/">
+                                <img src={imgEcommerceReact} alt="Vista previa app control escolar" target="_blank" rel="noopener noreferrer"></img>
+                            </a>
                         </div>
                         <div className="projectName">
-                            <h3>Angular Js</h3>
+                            <h3 className='mt-5 text-center'>App control escolar Angular</h3>
                         </div>
                     </div>
                 </div>
-                <div className="project col-2">
-                    <div className="projectName">
+                <div className="skill col-4">
+                    <div className="project">
                         <div className="projectImg">
+                            <a href="https://proyecto-final-gonzalez-coder.netlify.app/">
+                                <img src={imgEcommerceReact} alt="Vista previa eccomerce React" target="_blank" rel="noopener noreferrer"></img>
+                            </a>
                         </div>
                         <div className="projectName">
-                            <h3>HTML+CSS+Js</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className="project col-2">
-                    <div className="projectName">
-                        <div className="projectImg">
-                        </div>
-                        <div className="projectName">
-                            <h3>git</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className="project col-2">
-                    <div className="projectName">
-                        <div className="projectImg">
-                        </div>
-                        <div className="projectName">
-                            <h3>SASS</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className="project col-2">
-                    <div className="projectName">
-                        <div className="projectImg">
-                        </div>
-                        <div className="projectName">
-                            <h3>Bootstrap</h3>
+                            <h3 className='mt-5 text-center'>E-commerce React Js</h3>
                         </div>
                     </div>
                 </div>
